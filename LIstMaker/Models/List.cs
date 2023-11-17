3ac7677931bfedf8471ad9c7dc4b358e9ed08fb4
David Benkner <d.benkner@gmail.com>
2023-11-17 16:03:54 -0500

    Begun list and list items classes

---
+++ LIstMaker/Models/List.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LIstMaker.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
---
+++ LIstMaker/Models/ListItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LIstMaker.Models
+{
+    public class ListItem
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string ItemName { get; set; } = string.Empty;
+        public int Qty { get; set; } = 1;
+    }
+}
