d0bf790070bcfd931e7dcd6844e199146281394d
David Benkner <d.benkner@gmail.com>
2023-11-17 14:07:58 -0500

    Worked on users controller

--- LIstMaker/Program.cs
+++ LIstMaker/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using LIstMaker.Data;
+using LIstMaker.Controllers;
+using LIstMaker.DTOs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LIstMakerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DevDb") ?? throw new InvalidOperationException("Connection string 'LIstMakerContext' not found.")));
@@ -21,3 +23,5 @@ app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.MapControllers();
 
 app.Run();
+
+
